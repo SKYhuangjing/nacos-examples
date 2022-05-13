1277c62beefe1c94b5cf0746900717403708e38c
sky.huang <sky.huang@1data.info>
2022-05-13 21:25:57 +0800

    add configService wrong demo

--- nacos-spring-cloud-example/nacos-spring-cloud-config-example/src/main/java/com/alibaba/nacos/example/spring/cloud/controller/ConfigController.java
+++ nacos-spring-cloud-example/nacos-spring-cloud-config-example/src/main/java/com/alibaba/nacos/example/spring/cloud/controller/ConfigController.java
@@ -1,9 +1,17 @@
 package com.alibaba.nacos.example.spring.cloud.controller;
 
+import cn.hutool.core.util.ReflectUtil;
+import com.alibaba.nacos.api.config.ConfigService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.alibaba.cloud.nacos.NacosConfigManager;
+
+import javax.annotation.PostConstruct;
+
 
 @RestController
 @RequestMapping("/config")
@@ -13,11 +21,30 @@ public class ConfigController {
     @Value("${useLocalCache:false}")
     private boolean useLocalCache;
 
+    @Value("${useLocalKey:test}")
+    private String useLocalKey;
+
+
+    @Autowired
+    private NacosConfigManager nacosConfigManager;
+
     /**
      * http://localhost:8080/config/get
      */
-    @RequestMapping("/get")
+    @GetMapping("/get")
     public boolean get() {
         return useLocalCache;
+    }
+
+
+    @GetMapping("/getConfigNamespace")
+    public Object getKey() {
+        ConfigService configService = nacosConfigManager.getConfigService();
+        Object namespace = ReflectUtil.getFieldValue(configService, "namespace");
+
+        System.out.println("-------Namespace-------");
+        System.out.println(namespace);
+        System.out.println("-------Namespace-------");
+        return namespace;
     }
 }
